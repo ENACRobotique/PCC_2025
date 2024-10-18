3a278f714d76ec958fc61186bce38d7c5db5d310
ENAC-Robotique <robotique.enac@gmail.com>
2024-10-18 14:29:42 +0200

    Adapted Metro to pico libs

--- Metro.h
+++ Metro.h
@@ -1,28 +1,35 @@
-//Code copied from Metro Library
+//Code extracted from Metro Library
 //Copyright (c) 2013 thomasfredericks
 //Under MIT License
 
+//Edited to be compliant with pico lib in 2024 by EnacRobotique
+
 #ifndef METRO
 #define METRO
+#include "pico/time.h"
 
-class Metro {
+uint32_t  get_time_millis(){
+    return to_ms_since_boot(get_absolute_time());}
+
+uint32_t  get_time_micros(){
+    return to_us_since_boot(get_absolute_time());}
+
+// Chronometer/stopwatch library that counts the time passed since started.
+// Time is in milli-seconds
+class Metro_millis {
     public:
-        Metro(unsigned long millis_): interval_millis(millis_) {}
+        Metro_millis(uint32_t millis_): interval_millis(millis_) {}
         
-        void interval(unsigned long millis_){
+        void interval(uint32_t millis_){
             this->interval_millis = millis_;
         }
+        //returns True if the desired elapsed time is reached
         bool check(){
-            unsigned long now = millis();
+            uint32_t now = get_time_millis();
             if (interval_millis == 0){
                 previous_millis = now;
                 return true;
             }
-            //Serial2.print(now);
-            //Serial2.print(" ");
-            //Serial2.print(previous_millis);
-            //Serial2.print(" ");
-            //Serial2.println(interval_millis);
             if ((now-previous_millis) >= interval_millis){
                 
                 #ifdef NOCATCHUP
@@ -35,10 +42,44 @@ class Metro {
             return false;
         }
         void reset(){
-            this->previous_millis = millis();
+            this->previous_millis = get_time_millis();
         }
     private:
-        unsigned long previous_millis, interval_millis;
+        uint32_t previous_millis, interval_millis;
+};
+
+// Chronometer/stopwatch library that counts the time passed since started.
+// Time is in micro-seconds
+class Metro_micros {
+    public:
+        Metro_micros(uint32_t micros_): interval_micros(micros_) {}
+        
+        void interval(uint32_t micros_){
+            this->interval_micros = micros_;
+        }
+        //returns True if the desired elapsed time is reached
+        bool check(){
+            uint32_t now = get_time_micros();
+            if (interval_micros == 0){
+                previous_micros = now;
+                return true;
+            }
+            if ((now-previous_micros) >= interval_micros){
+                
+                #ifdef NOCATCHUP
+                previous_micros = now;
+                #else
+                previous_micros += interval_micros;
+                #endif
+                return true;
+            }
+            return false;
+        }
+        void reset(){
+            this->previous_micros = get_time_micros();
+        }
+    private:
+        uint32_t previous_micros, interval_micros;
 };
 
 #endif
--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 #include "encodeur.h"
 #include "moteur.h"
 
+#include "Metro.h"
+
 const uint ENC1_AB = ENC1_A; //10-11 and 19-20
 const uint ENC2_AB = ENC2_A; //10-11 and 19-20
 const uint MOT1A = MOT1_A;
@@ -13,7 +15,6 @@ const uint MOT1EN=MOT1_EN;
 Encodeur encodeur1(ENC1_AB);
 Moteur moteur1(MOT1A,MOT1B,MOT1EN);
 
-
 const uint MOT2A = MOT2_A;
 const uint MOT2B=MOT2_B;
 const uint MOT2EN=MOT2_EN;
@@ -21,6 +22,7 @@ const uint MOT2EN=MOT2_EN;
 Encodeur encodeur2(ENC2_AB);
 Moteur moteur2(MOT2A,MOT2B,MOT2EN);
 
+Metro_micros test(2000);
 int main() {
     stdio_init_all();
     encodeur1.init_encoder();
@@ -28,10 +30,12 @@ int main() {
     encodeur2.init_encoder();
     moteur2.init_motor();
     while(1){
-        printf("value 1  = %d  value 2 = %d \n",encodeur1.get_value(),encodeur2.get_value());
+        // printf("value 1  = %d  value 2 = %d \n",encodeur1.get_value(),encodeur2.get_value());
         moteur1.set_motor(-0.5);
         moteur2.set_motor(0.5);
-
+        if(test.check()){
+            printf(" %d us\n", get_time_micros());
+        }
 
     }
 }
